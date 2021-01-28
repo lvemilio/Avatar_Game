7f66f9216789f5abf7513c7fa4e187771d876946
EmilsVigants <goemok21@gmail.com>
2021-01-28 22:53:48 +0200

    Added comments

--- src/main/parties/benderParty.scala
+++ src/main/parties/benderParty.scala
@@ -6,12 +6,13 @@ import second_ord_classses.BenderClass
 
 class benderParty(currentMembers:List[BenderClass]) extends partyClass {
   var members: List[BenderClass] = currentMembers
-  var totalEnemyLvl:Double = 0
+  var totalEnemyLvl:Double = 0 // Total enemy levels are later used to proprtionally divide xp
   def fightWon(enemyParty:partyClass):Unit={
     for(enemy <- enemyParty.members){
       totalEnemyLvl+=enemy.level
     }
-  var LvlIncrementer:Double = totalEnemyLvl/members.length
+  var LvlIncrementer:Double = totalEnemyLvl/members.length //The theoretical "level" every member recieves is passed to gainXp
+                                                          //GainxP later records this theoretical "level" and gives experience according to that members level
   for (member <- this.members){
     member.gainXP(LvlIncrementer)
     }
